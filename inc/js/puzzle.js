202390b39e056db8d6df805c9c1db56eb9963791
haliaeetusvocifer <hazizsegun@gmail.com>
2017-12-13 12:51:44 +0100

    commit for bootstrap, css, js, puzzle.js, etc

---
+++ inc/js/puzzle.js
@@ -0,0 +1,67 @@
+/**
+* Function that is executed when the element is dragged. 
+**/
+function start(e) {
+  e.dataTransfer.effecAllowed = 'move'; //Define the effect as move (It is the default)
+  e.dataTransfer.setData("Text", e.target.id); // Catch the item that is going to move
+  e.target.style.opacity = '0.4'; 
+}
+
+/**
+* Function that is executed is finished to drag the element. 
+**/
+function end(e){
+  e.target.style.opacity = ''; // Restore the opacity of the element     
+  e.dataTransfer.clearData("Data");     
+}
+
+/**
+* Function that is executed when a draggable element enters the element from which it is called.
+**/
+function enter(e) {
+  return true;
+}
+
+/**
+* Function that is executed when a draggable element is on the element from which it is called. 
+* Returns false if the object can be dropped on that element and true otherwise.
+**/
+function over(e) {
+  if ((e.target.className == "contenedorPieza") || (e.target.id == "contenedorPiezas"))
+    return false;
+  else
+  return true;
+}
+    
+/**
+*Function that is executed when a draggable element is released on the element from which it is called. 
+**/
+function drop(e){
+  e.preventDefault(); // Prevents the default action of the dropped element from being executed.
+  var elementoArrastrado = e.dataTransfer.getData("Text");
+  e.target.appendChild(document.getElementById(elementoArrastrado)); // Place the element released on the element from which this function is called
+  comprobarPuzzle();
+}
+
+function comprobarPuzzle(){
+  if((document.getElementById('pieza1').parentNode.id=='uno') &&
+    (document.getElementById('pieza2').parentNode.id=='dos') &&
+    (document.getElementById('pieza3').parentNode.id=='tres') &&
+    (document.getElementById('pieza4').parentNode.id=='cuatro'))
+  {
+    alert('Felicidades, has hecho el puzzle.');
+  }
+}
+
+/**
+* Displays a warning message if the browser does not support Drag & Drop. (In Windows neither IE nor Safari support it)
+**/
+function comprobarnavegador() {
+  if( 
+    (navigator.userAgent.toLowerCase().indexOf('msie ') > -1) || 
+    ((navigator.userAgent.toLowerCase().indexOf('safari') > -1) && (navigator.userAgent.toLowerCase().indexOf('chrome') == -1)))
+  {
+    alert("Tu navegador no soporta correctamente las funciones Drag & Drop de HTML5. Prueba con otro navegador.");
+  }
+
+}
